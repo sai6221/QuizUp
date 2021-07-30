821ca30cf2765800aa704b8cd198316f90199fb4
Sai Reddy <sai6221@gmail.com>
2021-07-30 20:00:32 +0530

    Initial Commit

---
+++ QuizUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizUp
+//
+//  Created by Sai Reddy on 30/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
